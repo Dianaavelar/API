f7aea35a4770633c849388e8265623fe4a984326
Diana Lafuente <dianaavelarlafuente@gmail.com>
2023-06-17 22:04:17 -0300

    Foi implementado uma documentação no swagger

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./src/routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+
+
 
 
 const app = express()
+
 app.unsubscribe(morgan('dev'))
 app.unsubscribe(bodyParser.urlencoded({extended:false}))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json())
 app.use(cors())
 app.use(routes)

02eb0a8e79f2038006f0e4d1b50674072be9ae52
Diana Lafuente <dianaavelarlafuente@gmail.com>
2023-07-06 11:21:06 -0300

    Adicionado gitignore e criado arquivo swagger

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const routes = require('./src/routes')
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./src/routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 
 
 
 
-const app = express()
 
-app.unsubscribe(morgan('dev'))
-app.unsubscribe(bodyParser.urlencoded({extended:false}))
+
+app.unsubscribe(morgan('dev'));
+app.unsubscribe(bodyParser.urlencoded({extended:false}));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(express.json())
-app.use(cors())
-app.use(routes)
-
-
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
 
 
 app.listen(3000, () => {
-    console.log('Servidor Iniciado!')
-})
+    console.log('Servidor Iniciado na porta 3000!');
+});
